311f6ea316471e0d3e285b757495c34e276549a5
mohammed mirza <1ds22cb032@dsce.edu.in>
2025-10-13 09:23:52 +0530

    02:convex package

---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,18 @@
+import{query} from "./_generated/server";
+import { mutation } from "./_generated/server";
+export const getMany=query({
+    args:{},
+    handler:async(ctx)=>{
+        const users=await ctx.db.query("users").collect();
+        return users;
+    },
+});
+export const add=mutation({
+    args:{},
+    handler:async(ctx)=>{
+        const userId=await ctx.db.insert("users",{
+        name:"mirza",});
+        return userId;
+        },
+    });
+    
