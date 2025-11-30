8cc20548bb93f53c541eda9d61e64b14c6bdad5f
GaganHnd <gaganhandral1@gmial.com>
2025-11-30 19:43:20 +0530

    11: widget screen router

--- apps/widgets/modules/widget/ui/views/widget-view.tsx
+++ apps/widgets/modules/widget/ui/views/widget-view.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-// import { WidgetFooter } from "@/modules/widget/ui/components/widget-footer";
-// import { WidgetHeader } from "@/modules/widget/ui/components/widget-header";
+import { useAtomValue } from "jotai";
 import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
 
 interface Props  {
     organizationId: string;
 };
 
 export const WidgetView = ( {organizationId}: Props ) => {
+    const screen = useAtomValue(screenAtom);
+
+    const screenComponents = {
+        error: <p>TODO: ERROR</p>,
+        loading: <p>TODO: Loading</p>,
+        auth: <WidgetAuthScreen />,
+        voice: <p>TODO: Voice</p>,
+        inbox: <p>TODO: Inbox</p>,
+        selection: <p>TODO: Selection</p>,
+        chat: <p>TODO: Chat</p>,
+        contact: <p>TODO: Contact</p>,
+    }
+
     return (
         <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetAuthScreen />
-            {/* <WidgetFooter /> */}
+            {screenComponents[screen]}           
         </main>
     );
 };
